1d5aa59575c6ac60c6e422fa2fc497e1b6318813
Godslovelee <godslovelee@yahoo.com>
2021-12-26 23:34:15 +0100

    "reformatted network_request.dart"

--- lib/services/network_request.dart
+++ lib/services/network_request.dart
@@ -1,10 +1,9 @@
-
 import 'dart:convert';
 import 'package:api_with_flutter_river/model/photo-model.dart';
-List<PhotoModel>parse_photo(String responseData){
-    var list = json.decode(responseData) as List<dynamic>;
-    List<PhotoModel>photos = list.map((e) => PhotoModel.fromJson(e)).toList();
 
-    return photos;
+List<PhotoModel> parse_photo(String responseData) {
+  var list = json.decode(responseData) as List<dynamic>;
+  List<PhotoModel> photos = list.map((e) => PhotoModel.fromJson(e)).toList();
 
+  return photos;
 }
