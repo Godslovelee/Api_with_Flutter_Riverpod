d54edf89f27f14e8bb779c6ec1b6f40c4d38d92a
Godslovelee <godslovelee@yahoo.com>
2021-12-27 00:55:34 +0100

    "Added FutureProvider to photoState"

--- lib/services/network_request.dart
+++ lib/services/network_request.dart
@@ -17,7 +17,7 @@ Future<List<PhotoModel>> fetchPhotos()async{
     return compute(parse_photo, response.body);
   }
   throw Exception(
-    "Can\'t get Photoeeeeeeeeee"
+    "Can\'t get Photoeeeeeeeeeee"
   );
 
 
