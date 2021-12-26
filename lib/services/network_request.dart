b22f04d27e7ffa79a8fa8eab5b1b7577071fc951
Godslovelee <godslovelee@yahoo.com>
2021-12-26 23:31:10 +0100

    "var list(json)"

--- lib/services/network_request.dart
+++ lib/services/network_request.dart
@@ -1,8 +1,10 @@
 
 import 'dart:convert';
 import 'package:api_with_flutter_river/model/photo-model.dart';
-List<PhotoModel>parsephoto(String responseData){
+List<PhotoModel>parse_photo(String responseData){
     var list = json.decode(responseData) as List<dynamic>;
-    return null;
+    List<PhotoModel>photos = list.map((e) => PhotoModel.fromJson(e)).toList();
+
+    return photos;
 
 }
