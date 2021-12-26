1b636deb73b9428fd9e71c0d378adc90e8734b79
Godslovelee <godslovelee@yahoo.com>
2021-12-27 00:52:40 +0100

    "Added FutureProvider to photoState"

--- lib/components/state_controller.dart
+++ lib/components/state_controller.dart
@@ -3,5 +3,5 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:api_with_flutter_river/services/network_request.dart';
 
 final photoState = FutureProvider<List<PhotoModel>>((ref) async{
-  return FetchPhotos();
+  return fetchPhotos();
 }
