67ccf86b7992c6ed59cdc4e6052bb9eb4913c85f
Godslovelee <godslovelee@yahoo.com>
2021-12-27 17:55:34 +0100

    "imports"

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -1,3 +1,16 @@
 import 'package:riverpod/riverpod.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:riverpod/riverpod.dart';
+import 'package:api_with_flutter_river/components/state_controller.dart';
+import 'package:api_with_flutter_river/model/photo-model.dart';
+
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class MyHomePage extends ConsumerWidget{
+  @override
+  Widget build(BuildContext context, ScopedReader watch) {
+    AsyncValue<List<PhotoModel>>photos = watch(photoState);
+    return null;
+  }
+  }
