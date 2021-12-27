12320d42d432a0b130ed8c735b41e07a0d6eb568
Godslovelee <godslovelee@yahoo.com>
2021-12-27 17:48:19 +0100

    "added AsyncValue<List>"

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:api_with_flutter_river/components/state_controller.dart';
+import 'package:api_with_flutter_river/model/photo-model.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:api_with_flutter_river/services/network_request.dart';
@@ -18,7 +20,8 @@ class MyApp extends StatelessWidget {
   class MyHomePage extends ConsumerWidget{
   @override
   Widget build(BuildContext context, ScopedReader watch) {
-    AsyncValue<List<>
+    AsyncValue<List<PhotoModel>>photos = watch(photoState);
+    return null;
 
 
   }
