02a42933dec7904de579a1ad3458542b5f1ba36f
Godslovelee <godslovelee@yahoo.com>
2021-12-27 17:57:31 +0100

    "remove unused imports"

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
-import 'package:api_with_flutter_river/components/state_controller.dart';
-import 'package:api_with_flutter_river/model/photo-model.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:api_with_flutter_river/services/network_request.dart';
+
 
 void main() {
   runApp(ProviderScope(child: MyApp()));
