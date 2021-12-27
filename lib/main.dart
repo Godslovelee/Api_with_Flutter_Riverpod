f612faf6bb8c6d369d66344065866ab7508ddb27
Godslovelee <godslovelee@yahoo.com>
2021-12-27 17:59:28 +0100

    "remove unused imports"

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:api_with_flutter_river/ui/home.dart';
 
 
 void main() {
   runApp(ProviderScope(child: MyApp()));
 }
 
-class MyApp extends StatelessWidget {
-
-  @override
-  Widget build(BuildContext context) {
-
-    throw UnimplementedError();
-  }
-  }
 
 
