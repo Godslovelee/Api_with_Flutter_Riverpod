9afd22d10aed4b76a68ec2f28f9ffe661ac16630
Godslovelee <godslovelee@yahoo.com>
2021-12-27 20:55:47 +0100

    "refactor"

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -6,65 +6,48 @@ import 'package:api_with_flutter_river/model/photo-model.dart';
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'API-CONNECT',
-      theme: ThemeData(
-        primarySwatch: Colors.blue
-      ),
+      theme: ThemeData(primarySwatch: Colors.blue),
       home: MyHomePage(),
     );
-
-
   }
 }
 
-class MyHomePage extends ConsumerWidget{
+class MyHomePage extends ConsumerWidget {
   @override
   Widget build(BuildContext context, ScopedReader watch) {
-    AsyncValue<List<PhotoModel>>photos = watch(photoState);
+    AsyncValue<List<PhotoModel>> photos = watch(photoState);
     return Scaffold(
       backgroundColor: Colors.greenAccent,
       appBar: AppBar(
         title: Text("API"),
-
       ),
-      body: photos.when(data: (photos){
-        return ListView.builder(
-          itemCount: photos.length,
-          itemBuilder: (context, index){
-            return Column(
-              children: [
-                ListTile(
-                  leading: CircleAvatar(
-                    backgroundImage: NetworkImage(
-                      photos[index].thumbnailUrl
-
-                    ),
-
-
-                  ),
-                  title: Text(
-                    '${photos[index].title}'
-                  ),
-
-                )
-
-              ],
-            );
-
-
-          }
-
-        );
-      }, loading: () => Center(), error: (err, ss) => Center(
-        child: Text("cant load"),
-      )),
-
+      body: photos.when(
+          data: (photos) {
+            return ListView.builder(
+                itemCount: photos.length,
+                itemBuilder: (context, index) {
+                  return Column(
+                    children: [
+                      ListTile(
+                        leading: CircleAvatar(
+                          backgroundImage:
+                              NetworkImage(photos[index].thumbnailUrl),
+                        ),
+                        title: Text('${photos[index].title}'),
+                      )
+                    ],
+                  );
+                });
+          },
+          loading: () => Center(),
+          error: (err, ss) => Center(
+                child: Text("cant load"),
+              )),
     );
   }
-  }
+}
